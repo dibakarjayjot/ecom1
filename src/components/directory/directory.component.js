28cd4c81b0bbf79ef79aa850d6517f3d7f356c56
Janarthanan Seetharaman <janarthananseetharaman@Janarthanans-MacBook-Air.local>
2019-12-05 16:07:40 +0530

    routing implementation

--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -12,37 +12,42 @@ class Directory extends React.Component{
             sections:[{
                title:'hats',
                 id:1,
-                imageUrl:'https://i.ibb.co/cvpntL1/hats.png'
+                imageUrl:'https://i.ibb.co/cvpntL1/hats.png',
+                linkUrl:'hats'
             },
             {
                 title:'jackets',
                 id:2,
-                imageUrl:'https://i.ibb.co/px2tCc3/jackets.png'
+                imageUrl:'https://i.ibb.co/px2tCc3/jackets.png',
+                linkUrl:'hats'
             },
             {
                 title:'sneakers',
                 id:3,
-                imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png'
+                imageUrl:'https://i.ibb.co/0jqHpnp/sneakers.png',
+                linkUrl:'hats'
             },
         {
             title:'womens',
             id:4,
             imageUrl:'https://i.ibb.co/GCCdy8t/womens.png',
-            size:'large'
+            size:'large',
+            linkUrl:'hats'
         },
     {
         title:'mens',
         id:5,
         imageUrl:'https://i.ibb.co/R70vBrQ/men.png',
-        size:'large'
+        size:'large',
+        linkUrl:'hats'
     }]
         }
     }
     render(){
         return (
             <div className='directory-menu'>
-                { this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={ id } title={ title } imageUrl={imageUrl} size={size}/>
+                { this.state.sections.map(({id, ...otherSectionProps }) => (
+                    <MenuItem key={ id } {...otherSectionProps} />
                 ))}
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-ReactDom.render(<App/>, document.querySelector('#root'));
+ReactDom.render(
+<BrowserRouter>
+<App/>
+</BrowserRouter>, document.querySelector('#root'));
